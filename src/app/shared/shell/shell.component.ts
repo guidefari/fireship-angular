5c225662e3f2b4e205b4ebbffd4480d9a9acc0dc
Guide Fari <guideg6@gmail.com>
2021-05-30 00:00:40 +0200

    App Navigation Shell

--- src/app/shared/shell/shell.component.ts
+++ src/app/shared/shell/shell.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-shell',
@@ -7,7 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShellComponent implements OnInit {
 
-  constructor() { }
+  // TODO read into this code snippet below, good rxjs practice
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Handset])
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+  // end code snippet
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
   }
