02321c4b7064db46f4527d47a12e72c6a130f95f
Guide Fari <guideg6@gmail.com>
2021-05-31 06:14:35 +0200

    firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBJqNry1jwrAM-WsWeup_a4H4e6RILubDc",
+    authDomain: "fireship-c4ca9.firebaseapp.com",
+    projectId: "fireship-c4ca9",
+    storageBucket: "fireship-c4ca9.appspot.com",
+    messagingSenderId: "28013483801",
+    appId: "1:28013483801:web:63d1280edbae97c6405788",
+    measurementId: "G-9VV3D9CZJ0"
+  }
 };
