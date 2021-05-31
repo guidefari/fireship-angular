ed0f81fe2dc007394fba86e3b0ceb04ce235d5dd
Guide Fari <guideg6@gmail.com>
2021-05-31 06:14:35 +0200

    firebase setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBJqNry1jwrAM-WsWeup_a4H4e6RILubDc",
+    authDomain: "fireship-c4ca9.firebaseapp.com",
+    projectId: "fireship-c4ca9",
+    storageBucket: "fireship-c4ca9.appspot.com",
+    messagingSenderId: "28013483801",
+    appId: "1:28013483801:web:63d1280edbae97c6405788",
+    measurementId: "G-9VV3D9CZJ0"
+  }
 };
 
 /*
