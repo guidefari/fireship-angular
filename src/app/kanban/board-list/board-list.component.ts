19cd5dc192ba8a3e6b03df73691d76992a195448
Guide Fari <guideg6@gmail.com>
2021-06-11 19:56:46 +0200

    dialogs & delete button

--- src/app/kanban/board-list/board-list.component.ts
+++ src/app/kanban/board-list/board-list.component.ts
@@ -3,6 +3,8 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Subscription } from 'rxjs';
 import { Board } from '../board.model';
 import { BoardService } from '../board.service';
+import { MatDialog } from "@angular/material/dialog";
+import { BoardDialogComponent } from "../dialogs/board-dialog.component";
 
 @Component({
   selector: 'app-board-list',
@@ -13,12 +15,14 @@ export class BoardListComponent implements OnInit, OnDestroy {
   boards: Board[]
   sub: Subscription
 
-  constructor(public boardService: BoardService) { }
+  constructor(public boardService: BoardService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.sub = this.boardService
       .getUserBoards()
       .subscribe(boards => (this.boards = boards))
+
+      console.log(this.boards)
   }
 
   ngOnDestroy() {
@@ -29,5 +33,22 @@ export class BoardListComponent implements OnInit, OnDestroy {
     moveItemInArray(this.boards, event.previousIndex, event.currentIndex)
     this.boardService.sortBoards(this.boards)
   }
+
+  openBoardDialog(): void {
+    const dialogRef = this.dialog.open(BoardDialogComponent, {
+      width: '400px',
+      data: {  }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.boardService.createBoard({
+          title: result,
+          priority: this.boards.length
+        })
+      }
+    })
+  }
+
 
 }
